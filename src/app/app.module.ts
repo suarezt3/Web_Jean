6efb89c53bdeb2015ab8d6b7e619d35894af375c
Eyder <eyder.suarez@nivelics.co>
2022-02-22 21:14:58 -0500

    arreglo del componente shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedComponent } from './shared/shared.component';
 import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent,
     MenuComponent
   ],
   imports: [
