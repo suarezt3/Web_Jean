b1828cd92b70b71524fe346a7fbd51b41ebca7c0
Eyder <eyder.suarez@nivelics.co>
2022-02-22 21:10:15 -0500

    Inicio del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
